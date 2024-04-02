1e3bd695133e28b21e892450ed0c1f0b54528201
BP3-Tiger <staporn.thirasaroj@gmail.com>
2024-04-02 15:27:32 +0700

    edited from BP3-Tiger

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalAmount = 27000;
+let discountCalculate = totalAmount - (totalAmount * (25 / 100));
+let netAmount = discountCalculate;
+console.log(netAmount)
